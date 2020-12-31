ce07c97b66516cd383bbb1961f3a2df5238ecb3a
herbrant <cdavide98carnemolla@gmail.com>
2020-12-31 17:43:08 +0100

    feat(title): add routes titles

--- src/router/index.js
+++ src/router/index.js
@@ -8,22 +8,36 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'iFOIA'
+    }
   },
   {
     path: '/app',
     name: 'app',
-    component: () => import(/* webpackChunkName: "about" */ '../views/iFoia.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/iFoia.vue'),
+    meta: {
+      title: 'iFOIA - App'
+    }
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: 'iFOIA - About'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
 })
 
 export default router
